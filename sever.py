2bd988f9ef836c6a6a083e46e773f0e30822e4c4
yantingw <tina0405tina@gmail.com>
2019-06-12 20:00:18 +0800

    Update sever.py

--- sever.py
+++ sever.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 29 21:00:23 2019
-
-@author: YanTing
-"""
-
-
 import socket,threading
 import cv2
 import numpy
@@ -13,14 +5,17 @@ import pickle
 import sys
 import pyaudio
 import time
+TCP_IP = '10.129.207.29'
 
-#%%
+
 def check_ip(ipAd):
     try :
         with open('pickle_example.pickle', 'rb') as file:
-            ipdict = pickle.load(file)    
+            ipdict = pickle.load(file)
+            
         if ipAd in ipdict.keys():
-            ipdict[ipAd]+=1    
+            ipdict[ipAd]+=1
+            print(f"check ip number :{ipdict}")
         else:
             ipdict[ipAd] = 0
 
@@ -35,7 +30,7 @@ def check_ip(ipAd):
             pickle.dump(ipdict,file)
             print("make the pickle file")
 
-    return ipdict[ipAd] > 4000000000
+    return ipdict[ipAd] > 100
 
 
 def callback(in_data, frame_count, time_info, status):
@@ -85,46 +80,67 @@ def open_new_socket(conn,addr,num_of_client):
     conn, addr = s.accept()
     print("complete!!!")
     return conn,addr
-    
-def serve_the_client(conn,addr,num_of_client):
 
+
+def serve_the_client(conn,addr,num_of_client):
+    
     if check_ip(addr[0]):
         conn.send("request too frequent".encode())    
         conn.shutdown(socket.SHUT_RDWR)
         sys.exit("some error message")#can be removed
     else:
-        print("nice")
         conn,addr = open_new_socket(conn,addr,num_of_client) 
         strea = recor()
         print(f"the client addr is {addr}")
         conn.send("start".encode())
         ret, frame = capture.read()
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
-        print(ret)
+        current_pixel=720
+        estimated_rtt=0
+        alpha=0.125
         while ret:
+            if current_pixel == 480:
+                frame=cv2.resize(frame, (64,48))
+    #            print(f"send the diffrent img :   {frame.shape}")
+
             if conn.recv(5).decode() == "start":
                 sendt = time.time()
             conn.send(strea.read(2048))
             result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+     #       print(f"send orgin img :   {imgencode.shape}")           
             data = numpy.array(imgencode)
             stringData = data.tostring() 
-            print(len(stringData))
-            conn.send(str(len(stringData)).ljust(16).encode('utf-8'))
-            print(str(len(stringData)).encode('utf-8'))
+            
+                
+            conn.send(str(len(stringData)).ljust(16).encode())
+            
             conn.send(stringData)
             
-            #decimg = cv2.imdecode(data,1)
-            
+
+                        
 
             #"""cv2.imshow('SERVER2',decimg)"""
-            
-            if conn.recv(2).decode() == "ok":
+            mess= conn.recv(3).decode()
+            if mess == "ok!":
                 rtt_t = time.time() - sendt
-              
-                print('RTT time: {} seconds'.format(rtt_t), end="\r")
-            
-            cv2.waitKey(20)
+                if estimated_rtt == 0:
+                    estimated_rtt=rtt_t
+                estimated_rtt=(1-alpha)*estimated_rtt + alpha*rtt_t
+                cv2.waitKey(int(estimated_rtt*1000))
+                frame=cv2.resize(frame, (1080*(100),720*()))
+                print('RTT time: {} ms'.format(int(estimated_rtt*1000)), end="\r")
+            else:
+                
+                if mess == "480":
+                    current_pixel=480
+                    print('改變pixel480')
+                elif mess == "720":
+                    current_pixel=720
+                    print('改變pixel720')
+                
+                cv2.waitKey(100)
             ret, frame = capture.read()
+      #      print(ret)
 
         conn.close()
         cv2.destroyAllWindows()
@@ -136,7 +152,6 @@ def serve_the_client(conn,addr,num_of_client):
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
 #def main(): 
 
-TCP_IP = '192.168.0.102'
 with open('pickle_example.pickle', 'rb') as file:
         ipdict = pickle.load(file)    
         for ipAd in ipdict.keys():
@@ -148,17 +163,13 @@ with open('pickle_example.pickle', 'rb') as file:
             file.close()
 
 capture = cv2.VideoCapture(0)
-print(capture)
 chc = int(input("輸入希望像素(如 480, 720):"))
-
-
 num_of_client = 0
 if chc == 480:
     make_480p()
 else:
     make_720p()
 
-#TCP_IP = ''
 TCP_PORT = 6000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 s.bind((TCP_IP, TCP_PORT))
